7deba27297027233f5d3c182f5fd5e0b2b765f28
Andrii Zakharov <zaharov.andrey.93@gmail.com>
2025-06-26 19:38:45 +0300

    update input text color

--- src/components/FiltersRow/FiltersRow.tsx
+++ src/components/FiltersRow/FiltersRow.tsx
@@ -1,32 +1,32 @@
-import React from "react";
-import type { UsersState } from "../../types";
+import React from 'react'
+import type { UsersState } from '../../types'
 
-type FilterKey = keyof UsersState["filters"];
+type FilterKey = keyof UsersState['filters']
 
 interface FiltersRowProps {
-  filters: UsersState["filters"];
-  onChange: (key: FilterKey, value: string) => void;
+	filters: UsersState['filters']
+	onChange: (key: FilterKey, value: string) => void
 }
 
-const columns = ["name", "username", "email", "phone"] as const;
+const columns = ['name', 'username', 'email', 'phone'] as const
 
 export const FiltersRow: React.FC<FiltersRowProps> = ({
-  filters,
-  onChange,
+	filters,
+	onChange,
 }) => {
-  return (
-    <tr>
-      {columns.map((key) => (
-        <th key={key} className="has-background-info-dark">
-          <input
-            className="input is-medium has-text-dark is-info"
-            type="text"
-            placeholder={`Search ${key}`}
-            value={filters[key]}
-            onChange={(e) => onChange(key, e.target.value)}
-          />
-        </th>
-      ))}
-    </tr>
-  );
-};
+	return (
+		<tr>
+			{columns.map(key => (
+				<th key={key} className='has-background-info-dark'>
+					<input
+						className='input is-medium is-info'
+						type='text'
+						placeholder={`Search ${key}`}
+						value={filters[key]}
+						onChange={e => onChange(key, e.target.value)}
+					/>
+				</th>
+			))}
+		</tr>
+	)
+}
